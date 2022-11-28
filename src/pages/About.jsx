54318cd9dcb57be6b6b69191edf5a7990f4e33c6
Jennifer J <jenniferjuarezse@gmail.com>
2022-11-27 19:47:40 -0500

    add info in my About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,20 @@
 
-
-function About() {
+const About = (props) => {
     return (
-        <h1>About</h1>
-    )
-}
-
-export default About;
+      <div ref={props.aboutRef} className='aboutSection'>
+        <h1>About Me</h1>
+        {/* <img className='profileImage' src={Picture} alt="jennifer-profile" /> */}
+        <div className='aboutDesc'>
+          <p>
+            I am an aspiring software engineer and countinue to develop my skills both creatively and professionally. I would like to continue growing and learning all while keeping up with industry knowledge. My background in Business and as collegiate athlete makes me a business savy & team player.    </p>
+          <p>
+            In my personal time, coach Life Universities women's wrestling team to inspire young atheletes to be the best version of themselves. I am a very athletic and creative person who loves trying new things and making them.
+          </p>
+        </div>
+  
+  
+      </div>
+    );
+  }
+  
+  export default About;
