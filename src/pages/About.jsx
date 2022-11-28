4fa0dd0242a8082e8339a300cd60d3ea9d06b259
Jennifer J <jenniferjuarezse@gmail.com>
2022-11-28 18:27:31 -0500

    add information to Projects.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,7 +10,7 @@ const About = (props) => {
           <p>
             I am an aspiring software engineer and countinue to develop my skills both creatively and professionally. I would like to continue growing and learning all while keeping up with industry knowledge. My background in Business and as collegiate athlete makes me a business savy & team player.    </p>
           <p>
-            In my personal time, coach Life Universities women's wrestling team to inspire young atheletes to be the best version of themselves. I am a very athletic and creative person who loves trying new things and making them.
+            In my personal time, coach Life Universities women's wrestling team to inspire young atheletes to be the best version of themselves. I am a very athletic and creative person who loves trying new things. I am excited to keep learning and growing as a software engineer. 
           </p>
         </div>
   
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,8 +1,32 @@
+import Tictactoe from '../assets/tic-tac-toe.png'
+import War from '../assets/war-card-game.png'
+import CoolerCarsAndMotorcycles from '../assets/cooler-cars-and-motorcycles.png'
 
-function Projects() {
-    return (
-        <h1>Projects</h1>
-    )
-}
 
-export default Projects;
+const Projects = (props) =>[
+  {
+    title:"Tic Tac Toe ",
+    description:"This Tic Tac Toe app is an original take on the OG Tic Tac Toe game we all love playing with friends and family. You will be playing against a partner (you will need another person to play this game, I have not yet added computer funtionality). Play Tic Tac Toe as you orginally would with the original board game. Best of luck players!",
+    image: Tictactoe,
+    repositoryLink:"https://github.com/J3NNog1/ttt-weekend",
+    deploymentLink:"https://jenn-juarez-tic-tac-toe.netlify.app/"
+  },
+  {
+    title:"War Card Game",
+    description:"In this project a player can play the original War Card game! You will be playing against the computer. The functionality of war is already programmed where a users cards are automatically compared to the computers cards. From there the highest card wins and the winners hands gets the two cards that were just played. There is also functionality where if both the player and the computer hits a tie, War will automatically take affect(war means https://bicyclecards.com/how-to-play/war ). The perspective winner will take the cards. There is also double war functionality. Check out my GitHub to take a closer look at my code!",
+    image: War,
+    repositoryLink:"https://github.com/J3NNog1/war-card-game.git",
+    deploymentLink:"https://war-card-game-1.netlify.app"
+  },
+  {
+    title:"Cooler Cars and Motorcycles",
+    description:"Plan.e is an app that allows users to create trip plans using crowd-sourced user activities. By making travelling a collaborative process, we want to make the experience for each of our users great. Technologies: React, Full-Stack, CSS, JS, HTML",
+    image:CoolerCarsAndMotorcycles,
+    repositoryLink:"https://github.com/J3NNog1/cooler-cars-motorcycles.git",
+    deploymentLink:"flyio-cooler-cars-motorcycles.fly.dev"
+  },
+  
+
+]
+
+export default Projects
