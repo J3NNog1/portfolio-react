aa428019effb2085ad8dc96dd4be380e28885f25
Jennifer J <jenniferjuarezse@gmail.com>
2022-11-27 20:24:17 -0500

    add info to my Resume.jsx

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,9 +1,15 @@
+import ResumeFile from '../assets/jennifers-resume.pdf'
 
 
-function Resume() {
-    return (
-        <h1>Resume</h1>
-    )
+const Resume = (props) => {
+  return ( 
+    <div ref={props.resumeRef} id="resumeProfile">
+      <img src={Document} alt="document icon" />
+      <h1>Resume</h1>
+      <p>Click the link to download <a href={ResumeFile}>my resume</a></p>
+    </div>
+   );
 }
+ 
 
 export default Resume;
