bea9cc8647a3de7f0597bc43e3a1045682451bed
Jennifer J <jenniferjuarezse@gmail.com>
2022-11-27 20:10:49 -0500

    add information in Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,19 @@
 
 
-
-function Contact() {
+const Contact = (props) => {
     return (
-        <h1>Contact me!</h1>
-    )
-}
+      <div ref={props.contactRef} id="contactProfile">
+        <section>
+          <h1>Contact</h1>
+          <p>Feel free to contact me for any projects or oppurtunities that might succeed from our collaboration through <a href="mailto:jenniferjuarezse@gmail.com">email</a> </p>
+          <h3>Social Media</h3>
+          <ul>
+            <li><a href="https://www.linkedin.com/in/jennifer-juarez44/">LinkedIn</a></li>
+            <li><a href="https://github.com/J3NNog1">Github</a></li>
+          </ul>
+        </section>
+      </div>
+    );
+  }
 
 export default Contact;
