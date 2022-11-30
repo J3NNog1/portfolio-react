6c83020e3f88a7199a83c4a6af67ae9db5751d70
Jennifer J <jenniferjuarezse@gmail.com>
2022-11-30 17:56:58 -0500

    fix error in my app.jsx import

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,7 +8,7 @@ const About = (props) => {
         <img className='profileImage' src={AboutMe} alt="jennifer-profile" />
         <div className='aboutDesc'>
           <p>
-            I am an aspiring software engineer and countinue to develop my skills both creatively and professionally. I would like to continue growing and learning all while keeping up with industry knowledge. My background in Business and as collegiate athlete makes me a business savy & team player.    </p>
+            I am an aspiring software engineer who countinues to develop my skills both creatively and professionally. I would like to continue growing and learning all while keeping up with industry knowledge. My background in Business and as collegiate athlete makes me a business savy & team player.    </p>
           <p>
             In my personal time, coach Life Universities women's wrestling team to inspire young atheletes to be the best version of themselves. I am a very athletic and creative person who loves trying new things. I am excited to keep learning and growing as a software engineer. 
           </p>
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,11 +1,14 @@
 import ResumeFile from '../assets/jennifers-resume.pdf'
+import ScreenShot from '../assets/resume-page-1.png'
+import ScreenShot2 from '../assets/resume-page-2.png'
 
 
 const Resume = (props) => {
   return ( 
     <div ref={props.resumeRef} id="resumeProfile">
-      <img src={Document} alt="document icon" />
-      <h1>Resume</h1>
+      <img className='resumeImage' src={ScreenShot} alt='resume-screenshot' />
+      <img className='resumeImage' src={ScreenShot2} alt='resume-screenshot' />
+      <h1>Jennifer's Resume</h1>
       <p>Click the link to download <a href={ResumeFile}>my resume</a></p>
     </div>
    );
